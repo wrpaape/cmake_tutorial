9f38b697402bd2185c18480b1a7f8431e3521e97
Reid Paape <wrpaape@gmail.com>
2017-05-14 19:53:01 -0500

    unit testing frameworks

--- src/cmake_tutorial/test/test_cmake_tutorial.cpp
+++ src/cmake_tutorial/test/test_cmake_tutorial.cpp
@@ -1,50 +1,43 @@
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp" 
 #include "cmake_tutorial/cmake_tutorial.hpp"
 
-using std::cout;
-using std::endl;
+#include <vector>
 
-template<typename T, typename ...Args>
-void
-test_cmake_tutorial(const T &init_val,
-                    const T &set_val,
-                    const Args &...sum_vals)
+TEST_CASE( "vectors can be sized and resized", "[vector]" )
 {
-    T expected, got;
 
-    cout << "initializing value to: " << init_val << endl;
-
-    CmakeTutorial<T> ct(init_val);
-
-    got = ct.get();
-
-    cout <<   "expected: " << init_val
-         << "\ngot:      " << got << endl;
-
-    cout << "setting value to: " << set_val << endl;
-
-    ct.set(set_val);
-
-    got = ct.get();
-
-    cout <<   "expected: " << set_val
-         << "\ngot:      " << got << endl;
-
-    cout << "summing values" << set_val << endl;
-
-    expected = ct.do_sum(sum_vals...);
-    got      = ct.sum(sum_vals...);
-
-    cout <<   "expected: " << expected
-         << "\ngot:      " << got << endl;
-
-}
-
-
-int
-main()
-{
-    test_cmake_tutorial(1, 2, 3, 4, 5, 6);
-    test_cmake_tutorial(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
-    test_cmake_tutorial(1.0, 2.0, 3.0f, 4, 5ULL, 6.0);
-    return 0;
+    std::vector<int> v( 5 );
+    
+    REQUIRE( v.size() == 5 );
+    REQUIRE( v.capacity() >= 5 );
+    
+    SECTION( "resizing bigger changes size and capacity" )
+    {
+        v.resize( 10 );
+        
+        REQUIRE( v.size() == 10 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "resizing smaller changes size but not capacity" )
+    {
+        v.resize( 0 );
+        
+        REQUIRE( v.size() == 0 );
+        REQUIRE( v.capacity() >= 5 );
+    }
+    SECTION( "reserving bigger changes capacity but not size" )
+    {
+        v.reserve( 10 );
+        
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "reserving smaller does not change size or capacity" )
+    {
+        v.reserve( 0 );
+        
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 5 );
+    }
 }
